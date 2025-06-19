1cd0ba61c5fc5361ab20430f36afcf27fa805711
kayodeb <kayodebatcho@gmail.com>
2025-06-19 22:49:50 +0100

    last commit

--- arads_backend/models3d/admin.py
+++ arads_backend/models3d/admin.py
@@ -3,7 +3,7 @@ from .models import Model3D
 
 @admin.register(Model3D)
 class Model3DAdmin(admin.ModelAdmin):
-    list_display = ('name', 'qr_code','scan_count', 'created_at')
+    list_display = ('name', 'qr_code','scan_count', 'created_at','type')
     readonly_fields = ['scan_count']
     search_fields = ('name', 'qr_code')
     list_filter = ('created_at',)
